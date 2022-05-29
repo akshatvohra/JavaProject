9997cf78644de38001358abf609d89e8099a6cd6
akshatvohra@gmail.com <akshatvohra@gmail.com>
2022-05-29 19:27:28 +0530

    Added Loop.

--- myJava.java
+++ myJava.java
@@ -8,6 +8,10 @@ public class myJava
 		
 		int summation;
 		summation = number + numbeer;
+		for(int i=0; i<5; i++)
+		{
+			System.out.println("Loop Counter: "+i);
+		}
 		System.out.println("Summation of 2 numbers: "+summation);
 		System.out.println("This is a Java Program which is going to be executed on Jenkins!!");
 	}
