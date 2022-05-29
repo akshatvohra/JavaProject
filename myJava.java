0b3fdac4a88c8fef55a0eb952a6bb5dac4b0cfba
akshatvohra@gmail.com <akshatvohra@gmail.com>
2022-05-29 19:17:50 +0530

    Added Summation feature to the script.

--- myJava.java
+++ myJava.java
@@ -3,5 +3,12 @@ public class myJava
 	public static void main(String args[])
 	{
 		System.out.println("This is a Java Program which is going to be executed on Jenkins!!");
+		int number = 10;
+		int numbeer = 20;
+		
+		int summation;
+		summation = number + numbeer;
+		System.out.println("Summation of 2 numbers: "+summation);
+		System.out.println("This is a Java Program which is going to be executed on Jenkins!!");
 	}
 }
